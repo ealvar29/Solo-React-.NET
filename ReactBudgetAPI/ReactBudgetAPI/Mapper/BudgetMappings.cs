72bde5bff8f4c5619a1ffcd4f4a8eae84102d01c
Eduardo <ealvarez@carminaticonsulting.com>
2022-04-10 12:19:10 -0500

    configured auto mapper I think

---
+++ ReactBudgetAPI/ReactBudgetAPI/Mapper/BudgetMappings.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReactBudgetAPI.Models;
+using ReactBudgetAPI.Models.Dtos;
+
+namespace ReactBudgetAPI.Mapper
+{
+    public class BudgetMappings : Profile
+    {
+        public BudgetMappings()
+        {
+            CreateMap<Expense, ExpenseDto>().ReverseMap();
+        }
+    }
+}
