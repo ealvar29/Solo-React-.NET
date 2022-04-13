3e10b5c94da0a80a61389356198b75e95c8f7f57
Eduardo <edalvarez2015@gmail.com>
2022-04-13 12:09:50 -0500

    data not loading in yet

---
+++ ReactBudgetAPI/ReactBudgetAPI/ClientApp/src/components/Expense.js
@@ -0,0 +1,34 @@
+﻿import React, { useState } from 'react';
+
+export default function Expense() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("expense")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                    console.log(items)
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+    return (
+        <div>
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            {item.name} {item.cost}
+                        </li>
+                    ))}
+                </ul>
+        </div>
+    );
+}
