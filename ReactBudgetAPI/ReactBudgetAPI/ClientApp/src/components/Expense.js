ed8e74618c0ba48651f1d04ee70b1cd78d7206da
Eduardo <ealvarez@carminaticonsulting.com>
2022-04-15 16:12:25 -0500

    loaded up expense component

--- ReactBudgetAPI/ReactBudgetAPI/ClientApp/src/components/Expense.js
+++ ReactBudgetAPI/ReactBudgetAPI/ClientApp/src/components/Expense.js
@@ -1,34 +1,66 @@
-﻿import React, { useState } from 'react';
+﻿import React, { Component } from "react";
 
-export default function Expense() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+export class Expense extends Component {
+  static displayName = Expense.name;
 
-    useEffect(() => {
-        fetch("expense")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setItems(result);
-                    console.log(items)
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
+  constructor(props) {
+    super(props);
+    this.state = { forecasts: [], loading: true };
+  }
+
+  componentDidMount() {
+    this.populateWeatherData();
+  }
+
+    static renderForecastsTable(forecasts) {
+      console.log(forecasts)
     return (
-        <div>
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.name} {item.cost}
-                        </li>
-                    ))}
-                </ul>
-        </div>
+      <table className="table table-striped" aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map((forecast) => (
+            <tr key={forecast.date}>
+              <td>{forecast.name}</td>
+              <td>{forecast.cost}</td>
+              <td>{forecast.expenseType}</td>
+              <td>{forecast.dueDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
+  }
+
+  render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      Expense.renderForecastsTable(this.state.forecasts)
+    );
+
+    return (
+      <div>
+        <h1 id="tabelLabel" className="text-red-400">
+          Weather forecast
+        </h1>
+        <p>This component demonstrates fetching data from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+
+  async populateWeatherData() {
+    const response = await fetch("expense");
+    const data = await response.json();
+    this.setState({ forecasts: data, loading: false });
+  }
 }
