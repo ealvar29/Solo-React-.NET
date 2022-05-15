fb1c477cd37280faaa04992a09d97e159df22f00
Eduardo <ealvarez@carminaticonsulting.com>
2022-05-15 15:15:14 -0500

    integrating MUI into app

--- ReactBudgetAPI/ReactBudgetAPI/my-app/src/components/List.js
+++ ReactBudgetAPI/ReactBudgetAPI/my-app/src/components/List.js
@@ -1,8 +1,40 @@
-﻿import { useEffect, useState } from 'react'
+﻿import * as React from 'react';
 import moment from 'moment'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 function List(props) {
-    console.log(props)
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'firstName', headerName: 'First name', width: 130 },
+        { field: 'lastName', headerName: 'Last name', width: 130 },
+        {
+            field: 'age',
+            headerName: 'Age',
+            type: 'number',
+            width: 90,
+        },
+        {
+            field: 'fullName',
+            headerName: 'Full name',
+            description: 'This column has a value getter and is not sortable.',
+            sortable: false,
+            width: 160,
+            valueGetter: (params: GridValueGetterParams) =>
+                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        },
+    ];
+
+    const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    ];
 	return (
             <section className="py-1 bg-blueGray-50">
                 <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
@@ -46,7 +78,16 @@ function List(props) {
                             </table>
                         </div>
                     </div>
-                </div>
+            </div>
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
+            </div>
             </section>
 	);
 }
