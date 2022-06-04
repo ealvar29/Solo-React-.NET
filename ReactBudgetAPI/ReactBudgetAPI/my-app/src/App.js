fb66411da4248e889e843e3789b59eb984d10dc0
Eduardo <ealvarez@carminaticonsulting.com>
2022-06-04 15:49:14 -0500

    adding MUI into app part 2

--- ReactBudgetAPI/ReactBudgetAPI/my-app/src/App.js
+++ ReactBudgetAPI/ReactBudgetAPI/my-app/src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import List from './components/List'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 function App() {
@@ -12,6 +19,8 @@ function App() {
     const [newBillName, setNewBillName] = useState("");
     const [cost, setCost] = useState("0");
     const [type, setType] = useState("");
+    const [isPaid, setPaid] = useState(true);
+
 
     useEffect(() => {
         axios.get('/expense').then((response) => {
@@ -19,46 +28,119 @@ function App() {
             setLoading(false);
         }).catch(error => setError(error));
     }, []);
-    console.log(bills)
+
+
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (newBillName === "") return;
+
+        await supabase
+            .from("Expenses")
+            .insert({
+                name: newBillName,
+                cost: cost,
+                due_date: dueDate,
+                type: type,
+                isCompleted: isPaid,
+
+            })
+            .single()
+            .then(({ data, error }) => {
+                console.log(data, error);
+                if (!error) {
+                    setBills((prevBills) => [...prevBills, data]);
+                }
+            });
+
+        setNewBillName("");
+        setCost(0);
+        setDueDate("");
+        setType(false);
+    };
 
     if (isLoading) {
         return <div className="App">Loading...</div>;
     }
+    const toggleChange = (e) => {
+        setPaid(e.target.value);
+        console.log(isPaid)
+    };
+
+    const changeExpenseType = (e) => {
+        setType(e.target.value)
+    }
   return (
-      <div className="flex flex-wrap justify-evenly	items-baseline">
-          <form class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
-            <p class="text-gray-800 font-medium">Customer information</p>
-            <div class="">
-              <label class="block text-sm text-gray-00" for="cus_name">Name</label>
-              <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required="" placeholder="Your Name" aria-label="Name" />
+      <div className="grid grid-cols-1">
+          <form className="justify-center max-w-xl m-4 p-10 bg-white rounded shadow-xl mx-auto">
+              <p className="mt-4 text-gray-800 font-medium text-xlg">Budget Application</p>
+            <div className="">
+                  <label className="mt-4 text-gray-800 font-medium">Name</label>
+                  <input
+                      className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                      type="text"
+                      required=""
+                      placeholder="Your Name"
+                      aria-label="Name"
+                      onChange={(e) => {
+                          setError("");
+                          setNewBillName(e.target.value);
+                      }}
+                  />
             </div>
-            <div class="mt-2">
-              <label class="block text-sm text-gray-600" for="cus_email">Email</label>
-              <input class="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Your Email" aria-label="Email" />
+            <div className="mt-2">
+                <label className="mt-4 text-gray-800 font-medium">Cost</label>
+                <input
+                    className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                    required=""
+                    type="number"
+                    min="1" max="1000"
+                    placeholder="Cost"
+                    aria-label="Cost"
+                    onChange={(e) => {
+                        setError("");
+                        setCost(e.target.value);
+                    }}
+                />
             </div>
-            <div class="mt-2">
-              <label class=" block text-sm text-gray-600" for="cus_email">Address</label>
-              <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Street" aria-label="Email" />
-            </div>
-            <div class="mt-2">
-              <label class="hidden text-sm block text-gray-600" for="cus_email">City</label>
-              <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="City" aria-label="Email" />
-            </div>
-            <div class="inline-block mt-2 w-1/2 pr-1">
-              <label class="hidden block text-sm text-gray-600" for="cus_email">Country</label>
-              <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email" name="cus_email" type="text" required="" placeholder="Country" aria-label="Email" />
-            </div>
-            <div class="inline-block mt-2 -mx-1 pl-1 w-1/2">
-              <label class="hidden block text-sm text-gray-600" for="cus_email">Zip</label>
-              <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_email"  name="cus_email" type="text" required="" placeholder="Zip" aria-label="Email" />
-            </div>
-            <p class="mt-4 text-gray-800 font-medium">Payment information</p>
-            <div class="">
-              <label class="block text-sm text-gray-600" for="cus_name">Card</label>
-              <input class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="cus_name" name="cus_name" type="text" required="" placeholder="Card Number MM/YY CVC" aria-label="Name" />
-            </div>
-            <div class="mt-4">
-              <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">$3.00</button>
+              <div className="mt-2">
+                  <label className="mt-4 text-gray-800 font-medium my-5">Expense Type</label>
+                  <Box sx={{ minWidth: 120 }}>
+                      <FormControl variant="standard" fullWidth>
+                          <InputLabel id="demo-simple-select-label">Select Expense Type</InputLabel>
+                          <Select
+                              labelId="demo-simple-select-label"
+                              id="demo-simple-select"
+                              value={type}
+                              
+                              onChange={changeExpenseType}
+                          >
+                              <MenuItem value="Subscriptions">Subscriptions</MenuItem>
+                              <MenuItem value="Utilities">Utilities</MenuItem>
+                              <MenuItem value="Phone">Phone</MenuItem>
+                              <MenuItem value="Rent">Rent</MenuItem>
+                              <MenuItem value="Internet">Internet</MenuItem>
+                              <MenuItem value="Auto">Auto</MenuItem>
+                              <MenuItem value="Credit Card">Credit Card</MenuItem>
+                              <MenuItem value="Gas">Gas</MenuItem>
+                              <MenuItem value="Shopping">Shopping</MenuItem>
+                              <MenuItem value="Misc.">Misc.</MenuItem>
+                          </Select>
+                      </FormControl>
+                  </Box>
+              </div>
+              <p className="mt-4 text-gray-800 font-medium">Paid?</p>
+              <ToggleButtonGroup
+                  value={isPaid}
+                  exclusive
+                  onChange={toggleChange}
+                  className="my-2"
+              >
+                  <ToggleButton color="success" value="true">Paid</ToggleButton>
+                  <ToggleButton color="warning" value="false">Pending</ToggleButton>
+              </ToggleButtonGroup>
+            <div className="mt-4">
+                  <button className="px-4 py-1 text-white font-light tracking-wider bg-blue-600 rounded" type="submit" onClick={onSubmit}>Submit</button>
             </div>
           </form>
           <List bill={bills}/>
--- ReactBudgetAPI/ReactBudgetAPI/my-app/src/components/List.js
+++ ReactBudgetAPI/ReactBudgetAPI/my-app/src/components/List.js
@@ -1,40 +1,9 @@
 ﻿import * as React from 'react';
 import moment from 'moment'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 function List(props) {
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 90,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (params: GridValueGetterParams) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-    ];
-
-    const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
 	return (
             <section className="py-1 bg-blueGray-50">
                 <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
@@ -68,7 +37,22 @@ function List(props) {
                                                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">{item.name}</td>
                                                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">{item.cost}</td>
                                                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">{item.expenseType}</td>
-                                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">{item.isComplete.toString()}</td>
+                                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                                    {
+                                                        !item.isComplete ?
+                                                            <Stack spacing={1}>
+                                                                <Stack direction="row" spacing={1}>
+                                                                    <Chip label="Paid" color="success" />
+                                                                </Stack>
+                                                            </Stack>
+                                                            :
+                                                            <Stack spacing={1}>
+                                                                <Stack direction="row" spacing={1}>
+                                                                    <Chip label="Pending" color="warning" />
+                                                                </Stack>
+                                                            </Stack>
+                                                            }
+                                                </td>
                                                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">{moment(item.dueDate).format("MMM Do YYYY")}</td>
                                             </tr>
                                         )
@@ -78,15 +62,6 @@ function List(props) {
                             </table>
                         </div>
                     </div>
-            </div>
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                />
             </div>
             </section>
 	);
